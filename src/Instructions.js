7df82a1635246f88ed23039e099ecdfac58cb42b
Naveh Mevorach <navehmevorach@gmail.com>
2020-06-09 19:28:28 +0300

    emoji

---
+++ src/Instructions.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Instructions = () => {
+  return (
+    <>
+      <h1 className='article__title'>Instructions</h1>
+      <p className='article__text'>📃 Click on the New article</p>
+      <p className='article__text'>🎓 Read the article at loud</p>
+      <p className='article__text'> ✖ Repeat 3 times</p>
+      <p className='article__text'>
+        📆 Come back tomorrow for a new article or click again on the New
+        article button
+      </p>
+      <p className='article__text'>👉 Use the timer to check the time</p>
+      <p className='article__text'>
+        👉 Highlight a word or sentence for the translation and click on the
+        search icon
+      </p>
+      <p className='article__text'>
+        👉 Highlight a word or sentence for hearing it and click on the speaker
+        icon
+      </p>
+    </>
+  )
+}
+
+export default Instructions
--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,12 @@
 import React from 'react'
+import Instructions from './Instructions'
 
 const Main = () => {
   return (
     <main className='main'>
       <div className='main__content'>
         <article className='article'>
-          <h1 className='article__title'>Article</h1>
-          <p className='article__text'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-            alias minus consequuntur similique nulla adipisci aperiam, ea earum,
-            quibusdam non repellendus fugit ad harum atque, odio molestiae
-            corporis tenetur sit? Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Placeat exercitationem beatae perspiciatis natus
-            unde obcaecati odit voluptatibus porro temporibus esse sint mollitia
-            a minus delectus excepturi deleniti facere, ab est.
-          </p>
+          <Instructions/>
         </article>
       </div>
     </main>
