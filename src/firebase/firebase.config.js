9ff37ceee9b1a2f7e9f48faa63dbd4c6badbfe2d
Naveh Mevorach <navehmevorach@gmail.com>
2020-06-11 13:50:12 +0300

    security

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,10 +1,11 @@
 export default {
-   apiKey: "AIzaSyBgVFxGn956kv2pOCCkHwrgPvVtKjrEeqw",
-   authDomain: "fluentlly.firebaseapp.com",
-   databaseURL: "https://fluentlly.firebaseio.com",
-   projectId: "fluentlly",
-   storageBucket: "fluentlly.appspot.com",
-   messagingSenderId: "670247286085",
-   appId: "1:670247286085:web:87d6c090c217043dec3a2c",
-   measurementId: "G-BZ8GCLJNDX"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
+
